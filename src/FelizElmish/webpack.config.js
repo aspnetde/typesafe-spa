3919c082640ab17503840d7e8b69d8f97ac05af7
Thomas Bandt <bandt@69grad.de>
2020-06-23 23:10:57 +0200

    Add Feliz + Elmish sample app

--- src/FelizElmish/webpack.config.js
+++ src/FelizElmish/webpack.config.js
@@ -18,7 +18,7 @@ var CONFIG = {
     indexHtmlTemplate: "./src/index.html",
     fsharpEntry: "./src/App.fsproj",
     cssEntry: "./styles/main.scss",
-    outputDir: "./deploy",
+    outputDir: "../../docs/elmish-feliz",
     assetsDir: "./public",
     devServerPort: 8080,
     // When using webpack-dev-server, you may need to redirect some calls
