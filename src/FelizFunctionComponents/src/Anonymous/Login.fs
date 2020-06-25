8f5901b74c428f1a533cb17bffd1e26a5fdd6c27
Thomas Bandt <bandt@69grad.de>
2020-06-25 17:15:40 +0200

    Add login form

--- src/FelizFunctionComponents/src/Anonymous/Login.fs
+++ src/FelizFunctionComponents/src/Anonymous/Login.fs
@@ -3,5 +3,38 @@ module Login
 open Feliz
 
 let render = React.functionComponent(fun () ->
-    Html.text "Login"
+    Html.div [
+        Html.fieldSet [
+            Html.legend [ prop.text "Login" ]
+
+            Html.div [
+                //prop.text state.Error.Value
+                //prop.hidden state.Error.IsNone
+                prop.style [
+                    style.backgroundColor.red
+                    style.color.white
+                    style.fontWeight.bold
+                    style.padding 10
+                    style.marginBottom 15
+                ]
+            ]
+
+            Html.input [
+                prop.type'.text
+                prop.placeholder "User name"
+                //prop.onChange (UserNameSet >> dispatch)
+            ]
+            Html.br []
+            Html.input [
+                prop.type'.password
+                prop.placeholder "Password"
+                //prop.onChange (PasswordSet >> dispatch)
+            ]
+            Html.br []
+            Html.button [
+                //prop.onClick (fun _ -> dispatch StartLogin)
+                prop.text "Get in"
+            ]
+        ]
+    ]
 )
