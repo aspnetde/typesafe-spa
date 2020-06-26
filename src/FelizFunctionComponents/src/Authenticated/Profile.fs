21d43c91356459837485dac27847afdc04c14003
Thomas Bandt <bandt@69grad.de>
2020-06-26 23:24:44 +0200

    Implement the profile page

--- src/FelizFunctionComponents/src/Authenticated/Profile.fs
+++ src/FelizFunctionComponents/src/Authenticated/Profile.fs
@@ -1,7 +1,16 @@
 module Profile
 
+open AppNavigation
 open Feliz
 
 let render = React.functionComponent(fun () ->
-    Html.text "Profile"
+    let context = React.useContext AppContext.instance
+    Html.div [
+        Html.textf "Hello, %s! Go ahead and " context.Session.User
+        Html.a [
+            prop.text "edit"
+            prop.href (AuthenticatedUrl.EditProfile.Format())
+        ]
+        Html.text " your name."
+    ]
 )
