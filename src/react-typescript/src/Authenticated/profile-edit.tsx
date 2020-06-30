083c44ad07634506860f0c7e5ff2fd96f6dc9136
Thomas Bandt <bandt@69grad.de>
2020-06-30 17:37:51 +0200

    Implement profile update

--- src/react-typescript/src/Authenticated/profile-edit.tsx
+++ src/react-typescript/src/Authenticated/profile-edit.tsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import AppContext from "../AppContext";
 
 export default function EditProfile() {
-  return <div>Edit Profile</div>;
+  const appContext = React.useContext(AppContext.instance);
+  const [userName, setUserName] = React.useState(appContext.session.user);
+  let history = useHistory();
+
+  function update() {
+    appContext.setSession({ ...appContext.session, user: userName });
+    history.push("/app/profile");
+  }
+
+  return (
+    <div>
+      <label htmlFor="userName">User name:</label>
+      <br />
+      <input
+        type="text"
+        id="userName"
+        defaultValue={appContext.session.user}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <br />
+      <button onClick={update}>Update</button>
+    </div>
+  );
 }
