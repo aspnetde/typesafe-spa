e162f85100151aee56d68cc9aca052701ffa5675
Thomas Bandt <bandt@69grad.de>
2020-06-30 14:35:50 +0200

    Add basic login form

--- src/react-typescript/src/Anonymous/login.tsx
+++ src/react-typescript/src/Anonymous/login.tsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <fieldset>
+      <legend>Login</legend>
+      <div
+        style={{
+          backgroundColor: "red",
+          color: "white",
+          fontWeight: "bold",
+          padding: 10,
+          marginBottom: 15,
+        }}
+      >
+        Foo
+      </div>
+      <input type="text" placeholder="User name" />
+      <br />
+      <input type="password" placeholder="Password" />
+      <br />
+      <button>Get in</button>
+    </fieldset>
+  );
 }
