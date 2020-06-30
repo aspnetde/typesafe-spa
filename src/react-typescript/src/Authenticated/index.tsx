f067e230b4b95d97674e5010b4f04652d1c6886e
Thomas Bandt <bandt@69grad.de>
2020-06-30 16:50:19 +0200

    Implement logout

--- src/react-typescript/src/Authenticated/index.tsx
+++ src/react-typescript/src/Authenticated/index.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import AppContext from "../AppContext";
 import Dashboard from "./dashboard";
 import Profile from "./profile";
 import EditProfile from "./profile-edit";
 
 export default function Authenticated() {
+  const appContext = React.useContext(AppContext.instance);
+  const logout = () => {
+    appContext.setSession({ ...appContext.session, user: "" });
+  };
+
   return (
     <div>
       <Link to="/app/dashboard">Dashboard</Link> |{" "}
-      <Link to="/app/profile">Profile</Link>| <a href="#">Logout</a>
+      <Link to="/app/profile">Profile</Link>|{" "}
+      <a href="/" onClick={logout}>
+        Logout
+      </a>
       <hr />
       <Dashboard />
       <Profile />
